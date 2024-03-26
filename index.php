6cba6af210c70a5822432ed17e61a5aae3392307
EternityShards <162949914+EternityShards@users.noreply.github.com>
2024-03-26 09:45:59 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+function printUserInfo($name, $age) {
+    echo "<p>Имя: $name</p>";
+    echo "<p>Возраст: $age</p>";
+}
+
+// Пример использования функции
+printUserInfo("Иван", 25);
+
+?>
